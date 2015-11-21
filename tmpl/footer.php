59de6154b372b4361d5c0bada96ea8c7fd869a6d
Sequoia Snow <the1codemaster@gmail.com>
2015-11-21 14:44:02 -0500

    Some encryption file setup 2

--- tmpl/footer.php
+++ tmpl/footer.php
@@ -1,3 +1,8 @@
+<?php 
+/**
+ * Is included at the end of any template file and closes the opening container supplied in ./header.php
+ */
+?>
     <!-- Include scripts -->
     <script type="text/javascript" src="dist/js/all.js"></script>
     
